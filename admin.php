b658f3e3e728e6de7541d8ae141662eae754743d
ppl <leo.pichon@efrei.net>
2021-03-24 11:47:09 +0100

    contenu inaproprié

---
+++ admin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>FDP</title>
+</head>
+<body style="background-color:yellow; text-align: center;">
+<h1 style="color:fuchsia;">NIQUE </h1>
+<h1 style="color:red;">TON </h1>
+<h1 style="color:blue;">PERE</h1><br><br>
+<img src="logo.png"  height="15%" width="15%">
+</body>
+</html>
